21903631ff9276374f44b1d58c8f46987640ad45
UmairMirza1 <umair3215@gmail.com>
2022-10-24 21:27:30 +0500

    added widget image

--- src/Components/Area.js
+++ src/Components/Area.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from "react";
 
 export default function Area() {
   return (
-    <div>
+    <>
+      <div></div>
       Area
-    </div>
-  )
+    </>
+  );
 }
--- src/Components/Widget.js
+++ src/Components/Widget.js
@@ -1,9 +1,12 @@
-import React from 'react'
-
+import React from "react";
+import StarImage from "./star.png";
 export default function Widget() {
   return (
-    <div>
-      Widgets
-    </div>
-  )
+    <>
+      <div>
+        <p>Widget</p>
+        <img class="Wimage" src={StarImage} alt=""></img>
+      </div>
+    </>
+  );
 }
